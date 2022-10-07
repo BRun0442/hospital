ff3deed175010d6da8a62b4d9827a0350c10e0ae
Osmar Bruno <brunoosmar442@gmail.com>
2022-10-06 21:50:43 -0300

    Improving the header

--- front-end/src/Components/Header/index.jsx
+++ front-end/src/Components/Header/index.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import './index.css'
+import { Link } from "react-router-dom";
+// import { FaHospitalAlt } from "react-icons/fa";
 
 function index() {
   return (
     <header>
-      <img alt="Logo do site" src="" class="header-logo"/>
+      <div class="header-extendBar-button"><img src="https://img.icons8.com/ios-filled/50/000000/menu-rounded.png"/></div>
+      <Link to="/"><div class="header-logo" ><img alt="Logo do site" src="https://www.cmh.org/sites/default/files/CMH%20logo_0.png"/></div></Link>
+      {/* <FaHospitalAlt size="3rem"/> */}
 
-      <ul class="header-list">
-        <li class="header-list-item">Pacientes</li>
-        <li class="header-list-item">Para colaboradores</li>
-        <li class="header-list-item">Sobre</li>
-      </ul>
+      <nav>
+        <ul class="header-list">
+          <li class="header-list-item"><Link class="list-item-link" to="/pacientes">Pacientes</Link></li>
+          <li class="header-list-item"><Link class="list-item-link" to="/colabores">Colaboradores</Link></li>
+          <li class="header-list-item"><Link class="list-item-link" to="/sobre">Sobre</Link></li>
+        </ul>
+      </nav>
+
+      <nav class="header-button">
+        <button class="header-button-item"><Link to="">Para médicos</Link></button>
+        <button class="header-button-item"><Link to="">Para pacientes</Link></button>
+      </nav>
     </header>
   )
 }
