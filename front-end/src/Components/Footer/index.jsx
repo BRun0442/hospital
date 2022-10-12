42f12c56e7e53fe2edba0cbf566ea75c094ffa1f
Osmar Bruno <brunoosmar442@gmail.com>
2022-10-12 19:09:19 -0300

    Finishing the home page

--- front-end/src/Components/Footer/index.jsx
+++ front-end/src/Components/Footer/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./index.css";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+// import { FaLinkedin } from "react-icons/fa";
 
-function footer(props) {
+function footer() {
   return (
     <footer>
-      
+      <h2>
+        <em>"Sempre pensando em seu bem estar."</em>
+      </h2>
+      <div className="footer-icon-container">
+        <a href="https://github.com/BRun0442" target="blank">
+          <FaGithub size="2em" className="footer-icon-item" />
+        </a>
+        <a href="">
+          <FaInstagram size="2em" className="footer-icon-item" />
+        </a>
+        <a href="">
+          <FaYoutube size="2em" className="footer-icon-item" />
+        </a>
+        <a href="">
+          <FaLinkedin size="2em" className="footer-icon-item" />
+        </a>
+      </div>
     </footer>
   );
 }
--- front-end/src/Pages/Home/index.jsx
+++ front-end/src/Pages/Home/index.jsx
@@ -6,38 +6,35 @@ import "./index.css";
 
 function index() {
   return (
-    <div className="App">
+    <div className="app-home">
       <div className="bg-image"></div>
       <Header />
 
       <main>
         <h2>Cambridge</h2>
-
         <section className="card-container">
           <div className="card-item">
-            <Card 
+            <Card
               text="Atendimentos de qualidade, como você merece."
               image="https://retail.momentum.co.za/images/stock/hello-doctor-section-1-1.jpg"
             />
           </div>
-
           <div className="card-item">
-            <Card 
+            <Card
               text="Equipamentos de ponta."
               image="https://epomedicine.com/wp-content/uploads/2020/08/medical.jpg"
             />
           </div>
-
           <div className="card-item">
-            <Card 
+            <Card
               text="Possuímos os melhores colaboradores do Brasil."
               image="https://images.unsplash.com/photo-1638202993928-7267aad84c31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGRvY3RvcnxlbnwwfHwwfHw%3D&w=1000&q=80"
             />
           </div>
         </section>
       </main>
-
-      <Footer/>
+      
+      <Footer />
     </div>
   );
 }
